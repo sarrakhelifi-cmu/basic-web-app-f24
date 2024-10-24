35451d6c0e7037f3c0adf6edc7778680552d3578
seif khelifi <seifkhelifi@dyn-172-20-101-56.qatar.cmu.local>
2024-10-24 16:31:38 +0300

    add name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -12,7 +12,7 @@ export default function QueryProcessor(query: string): string {
     //TODO update the corresponding test case in __tests__
     return ( "skhelifi" );
   }
-  if (query.toLowerCase().includes("what is your name")) {
+  if (query.toLowerCase().includes("What is your name")) {
     // Add the correct response for the query "What is your name?"
     return "skhelifi-313";  // You can replace this with a specific name if needed.
   }
